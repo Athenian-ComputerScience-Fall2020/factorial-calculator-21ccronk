5fd075447c8820037e7ddc68dd4be3495df5c29b
21ccronk <21ccronk@athenian.org>
2020-09-20 20:29:20 +0000

    Stuck

--- my_code.py
+++ my_code.py
@@ -1,16 +1,21 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 # None
+# I know this is not much. I was trying to think through it and kept getting stuck.
 
 def factorial_calc(x):   #you may choose the name of the parameter
+    while x > 0:
+        y = x - 1
+        fac = x * y
+        return y
 
-    return    # be sure to return the factorial
+    return fac   # be sure to return the factorial
 
 
 if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(factorial_calc(5))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #num = input("Enter value to factorialize: ")
-    #print(factorial_calc(int(num)))
+    #num = int(input("Enter a value to factorialize: "))
+
+   
+    print(factorial_calc(4))
+
+    
