875f2b28b21a834893f48c3a71dc98ba327317ce
21ccronk <21ccronk@athenian.org>
2020-09-22 22:08:15 +0000

    Done!

--- my_code.py
+++ my_code.py
@@ -1,21 +1,21 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-# None
-# I know this is not much. I was trying to think through it and kept getting stuck.
+# Help from Megan
 
 def factorial_calc(x):   #you may choose the name of the parameter
+    fac = 1
     while x > 0:
-        y = x - 1
-        fac = x * y
-        return y
+        fac = fac * x
+        x = x - 1
+    return fac
 
-    return fac   # be sure to return the factorial
+       # be sure to return the factorial
 
 
 if __name__ == '__main__':
 
-    #num = int(input("Enter a value to factorialize: "))
+    x = int(input("Enter a value to factorialize: "))
 
    
-    print(factorial_calc(4))
+    print(factorial_calc(x))
 
     
